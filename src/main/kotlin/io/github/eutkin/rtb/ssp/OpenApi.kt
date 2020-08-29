ad7138d0f4599a3befaeb036918ceb031baeec82
e.utkin <e.utkin@bi.zone>
2020-08-29 15:54:19 +0300

    #14: CI
    Refactored open api spec. Removed spec desc from code and transfer to separated files (additional-schema.yml) because open api lib ignored description and example of product's field

---
+++ src/main/kotlin/io/github/eutkin/rtb/ssp/OpenApi.kt
@@ -0,0 +1,13 @@
+package io.github.eutkin.rtb.ssp
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
+
+@OpenAPIDefinition(
+        info = Info(
+                title = "ssp",
+                version = "0.1"
+        )
+)
+object OpenApi {
+}
--- src/main/kotlin/io/github/eutkin/rtb/ssp/impl/entrypoint/Product.kt
+++ src/main/kotlin/io/github/eutkin/rtb/ssp/impl/entrypoint/Product.kt
@@ -1,57 +1,17 @@
 package io.github.eutkin.rtb.ssp.impl.entrypoint
 
 import io.github.eutkin.rtb.ssp.impl.lot.ProductType
-import io.swagger.v3.oas.annotations.media.Schema
 import java.math.BigDecimal
 
-@Schema(
-        description = "Supplied product.",
-        accessMode = Schema.AccessMode.WRITE_ONLY
-)
 data class Product(
-        @Schema(
-                description = "Product description. Translate to all bidder.",
-                required = true,
-                example = """
-                    {
-                        "game": "FIFA07"
-                    }
-                """
-        )
+
         val description: Map<String, Any>,
 
-        @Schema(
-                description = "Secret product part. Translate to auction winner.",
-                required = true,
-                example = """
-                    {
-                        "code": "SUMMER2007"
-                    }
-                """
-        )
         val value: Map<String, Any>,
 
-        @Schema(
-                description = "Type of product. Should be a previously agreed constant",
-                example = "promocode",
-                required = true
-        )
         val productType: ProductType,
 
-        @Schema(
-                description = "Minimal price.",
-                defaultValue = "0",
-                example = "109.9"
-        )
         val price: BigDecimal = BigDecimal.ZERO,
 
-        @Schema(
-                description = """
-                   Degree of product quality relative to other products in the set. 
-                   Default value is minimal price 
-                """",
-                example = "10",
-                defaultValue = "109.9"
-        )
         val quality: Double = price.toDouble()
 )
